63524e80b30d7dcea93025089b5d121434157b99
pawelkleszko <kleszkopawel@gmail.com>
2020-07-26 19:46:52 +0200

    course copy - section 8

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var createError = require('http-errors');
+var cookieSession = require('cookie-session');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config');
+var mongoose = require('mongoose');
 
-const indexRouter = require('./routes/index');
-const newsRouter = require('./routes/news');
-const quizRouter = require('./routes/quiz');
-const adminRouter = require('./routes/admin');
+mongoose.connect(config.db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+var indexRouter = require('./routes/index');
+var newsRouter = require('./routes/news');
+var quizRouter = require('./routes/quiz');
+var adminRouter = require('./routes/admin');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -23,18 +34,23 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name: 'session',
+  keys: config.keySession,
+  maxAge: config.maxAgeSession
+}))
 
 app.use(function (req, res, next) {
   res.locals.path = req.path;
 
   next();
-})
+});
 
 app.use('/', indexRouter);
 app.use('/news', newsRouter);
 app.use('/quiz', quizRouter);
 app.use('/admin', adminRouter);
-
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
